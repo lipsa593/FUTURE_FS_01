2e873c4421d6791fdec8458d15eee72824968f2c
lipsa593 <porellipsa7@gmail.com>
2025-05-04 14:37:18 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Project from './components/Project';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <div className='bg-gradient-to-b'>
+      <div className='hero-container container mx-auto px-30 flex flex-col'>
+        <Header />
+        <Hero />
+        <contain/>
+      </div>
+      <div id="about" className='about-container container mx-auto px-30 flex flex-col'>
+        <About />
+      </div>
+      <div id="experience" className='experience-container container mx-auto px-30 flex flex-col'>
+        <Experience />
+      </div>
+      <div id="project" className='project-container container mx-auto px-30 flex flex-col'>
+        <Project />
+      </div>
+      <div id="contact" className='contact-container container mx-auto px-30 flex flex-col'>
+        <Contact />
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
