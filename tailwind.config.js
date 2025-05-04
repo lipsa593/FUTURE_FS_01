e17cf955f48cb4906c5f32b2b174a67a961f84a7
lipsa593 <porellipsa7@gmail.com>
2025-05-04 14:34:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all component files
+    "./src/index.css", // Include the CSS file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
